fde0565d7ac7f8b9d11e048ebe786a59140dfca0
p-p3 <pedro.alvarenga.afonso@gmail.com>
2024-08-24 19:21:08 -0300

    .

--- script/inject-navbar.js
+++ script/inject-navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => 
 {
-    fetch('/components/navbar.html')
+    fetch('/portfolio/components/navbar.html')
         .then(response => response.text())
         .then(navbarHTML => {
             const navCont = document.createElement('div');
