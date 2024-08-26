7fdf0cf94ad981d50e534bb22e440ee10e1c510d
p-p3 <pedro.alvarenga.afonso@gmail.com>
2024-08-26 17:50:50 -0300

    responsive navbar (code not cleaned yet)

---
+++ script/navbar.js
@@ -0,0 +1,38 @@
+/*   INJECT HTML TO PREVENT CODE DUPLICATION (CLEAN CODE BASED)   */
+
+document.addEventListener('DOMContentLoaded', () => 
+{
+    fetch('/components/navbar.html')
+        .then(response => response.text())
+        .then(navbarHTML => {
+            const navCont = document.createElement('div');
+            navCont.innerHTML = navbarHTML;
+            
+            document.body.insertBefore(navCont, document.body.firstChild);
+            navbarManager();
+        })
+        .catch(error => window.alert("Algo deu errado. Volte Novamente mais tarde." + error));     
+});
+
+
+
+const navbarManager = () =>
+{
+    const navmobile_button = document.getElementById('navbar-mobile__button');
+    const navmobile_content = document.getElementById('navbar-mobile__items');
+
+    navmobile_button.addEventListener("click", (ev) => 
+    {
+        navmobile_content.classList.toggle("active");
+        ev.stopPropagation();
+    });
+            
+
+    window.addEventListener("click", (ev) => 
+    {
+        if (!navmobile_content.contains(ev.target) && !navmobile_button.contains(ev.target))
+        {
+            navmobile_content.classList.remove("active");
+        }
+    });
+}
